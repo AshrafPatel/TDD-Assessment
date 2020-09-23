47fbdcd95b8d2dc61bcdabb4fc0efd52d3e3963b
Ashraf Patel <4shr4f@live.co.uk>
2020-09-23 13:52:07 +0100

    Added a few more tests

--- assessment/src/App.test.js
+++ assessment/src/App.test.js
@@ -3,9 +3,31 @@ import {mount} from "enzyme"
 import App from "../src/App"
 import { render } from '@testing-library/react';
 
-describe("App", () => {
+describe("App Headline", () => {
     it("renders currency converter", () => {
-        const wrapper = mount(<App/>)
-        expect(wrapper.find("h1").text()).toContain("Currency Converter")
+      const wrapper = mount(<App/>)
+      expect(wrapper.find("h1").text()).toContain("Currency Converter")
     })
 })
+
+describe("App Currency Form", () => {
+  it("renders a currency form", () => {
+    const wrapper = mount(<App/>)
+    expect(wrapper.contains("<div className='currency-form-container'>")).to.equal(true);
+  })
+})
+
+describe("App Date Form", () => {
+  it("renders a date form", () => {
+    const wrapper = mount(<App/>)
+    expect(wrapper.contains("<div className='date-form-container'>")).to.equal(true);
+  })
+})
+
+describe("App Log Date", () => {
+  it("renders a log", () => {
+    const wrapper = mount(<App/>)
+    expect(wrapper.contains("<div className='log-form-container'>")).to.equal(true);
+  })
+})
+
